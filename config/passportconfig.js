b904bbd8c43e35b4391b28a7c456c12f9585fb5a
JUN SU <chuing0120@naver.com>
2016-03-10 17:48:12 +0900

    오타 정보수정

--- config/passportconfig.js
+++ config/passportconfig.js
@@ -1,30 +1,31 @@
 var LocalStrategy = require('passport-local').Strategy;
-var SoundCloudStrategy  = require('passport-soundcloud').Strategy;
+var SoundCloudStrategy = require('passport-soundcloud').Strategy;
 var bcrypt = require('bcrypt');
 var async = require('async');
 var authconfig = require('./authconfig');
 
-module.exports = function(passport) {
+module.exports = function (passport) {
 
-	passport.serializeUser(function(user, done) {
+	passport.serializeUser(function (user, done) {
 		done(null, user.id);
 	});
 
-	passport.deserializeUser(function(id, done) {
-		pool.getConnection(function(err, connection) {
+	passport.deserializeUser(function (id, done) {
+		pool.getConnection(function (err, connection) {
 			if (err) {
 				done(err);
 			} else {
 				var sql = "SELECT id, username, nickname, cloud_id, cloud_token " +
-									 "FROM matchdb.user " +
-						       "WHERE id = ?";
-				connection.query(sql, [id], function(err, results) {
+					"FROM matchdb.user " +
+					"WHERE id = ?";
+				connection.query(sql, [id], function (err, results) {
 					connection.release();
 					if (err) {
 						done(err);
 					} else {
 						var user = {
 							"id": results[0].id,
+							"cloudId": results[0].cloud_id,
 							"username": results[0].username,
 							"nickname": results[0].nickname
 						};
@@ -34,13 +35,13 @@ module.exports = function(passport) {
 			}
 		});
 	});
+
 	// 2. 로컬 로그인 (HTTPS)
-	passport.use('local-login', new LocalStrategy({ // new LocalStrategy를 local-login로 정책한다.
+	passport.use('local-login', new LocalStrategy({
 		usernameField: "username",
 		passwordField: "password",
 		passReqToCallback: true
-	}, function(req, username, password, done) {
-
+	}, function (req, username, password, done) { // new LocalStrategy를 local-login로 정책한다.
 		function getConnection(callback) {
 			pool.getConnection(function (err, connection) {
 				if (err) {
@@ -50,6 +51,7 @@ module.exports = function(passport) {
 				}
 			});
 		}
+
 		function selectUser(connection, callback) {
 			var sql = "SELECT id, username, password " +
 				"FROM matchdb.user " +
@@ -73,6 +75,7 @@ module.exports = function(passport) {
 				}
 			});
 		}
+
 		function compareUserInput(user, callback) {
 			bcrypt.compare(password, user.hashPassword, function (err, result) {
 				if (err) {
@@ -86,7 +89,8 @@ module.exports = function(passport) {
 				}
 			});
 		}
-		async.waterfall([getConnection, selectUser, compareUserInput], function(err, user) {
+
+		async.waterfall([getConnection, selectUser, compareUserInput], function (err, user) {
 			if (err) {
 				done(err);
 			} else {
@@ -95,42 +99,35 @@ module.exports = function(passport) {
 			}
 		});
 	}));
-
-
-	 //15. 연동로그인 (HTTPS)
-	passport.use('soundcloud', new SoundCloudStrategy({ // new SoundCloudStrategy를 soundcloud로 정책한다.
-		"clientID": authconfig.soundcloud.appId, // appId
-		"clientSecret": authconfig.soundcloud.appSecret, // appSecret
-//		"profileFields": ["id", "displayName", "email", "photos"], // 프로필 원하는정보
-		"callbackURL" : authconfig.soundcloud.callbackURL, // callbackURL
-//		"passReqToCallback": true
-	},function(/*req,*/ accessToken, refreshToken, profile, done) {
-
-		console.log('acc',accessToken);
-		console.log('ssssss',profile);
-
-		function getConnection(callback) {
-			pool.getConnection(function (err, connection) {
-				if (err) {
-					callback(err);
-				} else {
-					callback(null, connection);
-				}
-			});
-		}
-		function findOrCreate(connection, callback) {
-			var sql = "SELECT id, cloud_id "+
-									"FROM matchdb.user " +
-									"WHERE cloud_id = ?";
-			connection.query(sql, [profile.id], function (err, results) {
-				if (err) {
-					connection.release();
-					callback(err);
-				} else {
-					if (results.length === 0) { //사용자가 존재 하지않을때 insert 기능일어남
-						var insert = "INSERT INTO +matchdb.user (cloud_id, cloud_token, nickname) " +
-													"VALUES (?, ?, ?)";
-						connection.query(insert, [profile.id, accessToken, profile._json.username], function (err, result) {
+	//15. 연동로그인 (HTTP)
+	passport.use('soundcloud', new SoundCloudStrategy({
+		"clientID": authconfig.soundcloud.appId,
+		"clientSecret": authconfig.soundcloud.appSecret,
+		"callbackURL": authconfig.soundcloud.callbackURL,
+		"passReqToCallback": true
+	}, function (req, accessToken, refreshToken, profile, done) {
+			function getConnection(callback) {
+				pool.getConnection(function (err, connection) {
+					if (err) {
+						callback(err);
+					} else {
+						callback(null, connection);
+					}
+				});
+			}
+			function selectOrCreateUser(connection, callback) {
+				var sql = "SELECT id, cloud_id " +
+					"FROM matchdb.user " +
+					"WHERE cloud_id = ?";
+				connection.query(sql, [profile.id], function (err, results) {
+					if (err) {
+						connection.release();
+						callback(err);
+					} else {
+						if (results.length === 0) { //사용자가 존재 하지않을때 insert 기능일어남
+							var insert = "INSERT INTO matchdb.user (cloud_id, cloud_token, nickname) " +
+								"VALUES (?, ?, ?)";
+							connection.query(insert, [profile.id, accessToken, profile._json.username], function (err, result) {
 								connection.release();
 								if (err) {
 									callback(err);
@@ -143,20 +140,20 @@ module.exports = function(passport) {
 									callback(null, user);
 								}
 							});
-					} else {
-						if (accessToken === results[0].cloud_token) { // 기존토큰과 새로들어온 토큰이 같을때
-							connection.release();
-							var user = {
-								"id": results[0].id,
-								"cloudId": results[0].cloud_id,
-								"cloudUsername": profile.username
-							};
-							callback(null, user);
-						} else { // 기존토큰과 새로들어온 토큰이 다를때
-							var update = "UPDATE matchdb.user " +
-														"SET cloud_token = ? " +
-														"WHERE cloud_id = ?";
-							connection.query(update, [accessToken, profile.id], function (err, result) {
+						} else {
+							if (accessToken === results[0].cloud_token) {
+								connection.release();
+								var user = {
+									"id": results[0].id,
+									"cloudId": results[0].cloud_id,
+									"cloudUsername": profile.username
+								};
+								callback(null, user);
+							} else {
+								var update = "UPDATE matchdb.user " +
+									"SET cloud_token = ? " +
+									"WHERE cloud_id = ?";
+								connection.query(update, [accessToken, profile.id], function (err, result) {
 									connection.release();
 									if (err) {
 										callback(err);
@@ -168,17 +165,17 @@ module.exports = function(passport) {
 										callback(null, user);
 									}
 								});
+							}
 						}
 					}
+				});
+			}
+			async.waterfall([getConnection, selectOrCreateUser], function (err, user) {
+				if (err) {
+					done(err);
+				} else {
+					done(null, user);
 				}
 			});
-		}
-		async.waterfall([getConnection, findOrCreate], function(err, user) {
-			if (err) {
-				done(err);
-			} else {
-				done(null, user);
-			}
-		});
 	}));
 };
