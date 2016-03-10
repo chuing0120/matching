23c48ae3465541265e992e869ec99c923b50aabd
JUN SU <chuing0120@naver.com>
2016-03-10 18:10:01 +0900

    사운드 클라우드 authconfig

---
+++ config/authconfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+	"soundcloud": {
+		"appId": process.env.FMS_SC_ID,
+		"appSecret": process.env.FMS_SC_SECRET,
+		"callbackURL": "http://localhost/auth/soundcloud/callback"
+	}
+}
+// 안드나 서버가 가져올때 쓰시길..
