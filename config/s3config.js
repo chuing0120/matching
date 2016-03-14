4997a4006d5dda8175411d07d273a7d40693966b
JUN SU <chuing0120@naver.com>
2016-03-14 14:55:11 +0900

    정리

--- config/s3config.js
+++ config/s3config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "key" : process.env.FMS_S3_KEY,
   "secret" : process.env.FMS_S3_SECRET,
-  "region" : "ap-northeast-2",
+  "region" : "ap-northeast-2", //-1은 도쿄, -2는 서울을 의미한다.
   "bucket" : process.env.FMS_S3_BUCKET,
   "imageDir" : process.env.FMS_S3_IMAGEDIR,
   "imageACL" : "public-read"
